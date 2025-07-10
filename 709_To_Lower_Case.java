a39faee4ba4cdee651bc2959e2ea8d380d471873
ARJUNKARTHIK S V <arjunkarthik5757@gmail.com>
2025-07-10 10:14:33 +0530

    Create 709_To_Lower_Case.java

---
+++ 709_To_Lower_Case.java
@@ -0,0 +1,22 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+// Example 2:
+
+// Input: s = "here"
+// Output: "here"
+// Example 3:
+
+// Input: s = "LOVELY"
+// Output: "lovely"
+
+//PROGRAM
+class Solution {
+    public String toLowerCase(String s) {
+        String str = s.toLowerCase();
+        return str;
+    }
+}
